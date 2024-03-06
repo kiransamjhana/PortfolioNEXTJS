c05a315473104b5c0081f5d971a865ac25684d85
kiransamjhana <kiranmanandhar956@gmail.com>
2024-03-06 19:39:12 +1100

    portfolio building

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
+          mannadharkiran90@gmail.com
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
