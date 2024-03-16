3b3beb03c47feed4ce8cfe719a97e96b039f2f6a
kiransamjhana <kiranmanandhar956@gmail.com>
2024-03-17 08:45:13 +1100

    react

--- next.config.js
+++ next.config.js
@@ -2,21 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-
-const path = require("path");
-const { NormalModuleReplacementPlugin } = require("webpack");
-
-webpack: (
-  config,
-  { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-) => {
-  config.plugins = config.plugins || [];
-  config.plugins.push(
-    new NormalModuleReplacementPlugin(
-      /email\/render/,
-      path.resolve(__dirname, "./renderEmailFix.js")
-    )
-  );
-  // Important: return the modified config
-  return config;
-};
